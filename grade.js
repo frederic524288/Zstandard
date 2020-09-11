7e5a6bac93fe3d23f3b23761ac3399c504e9f73b
CI Grader <zhezhou@umich.edu>
2020-09-11 13:06:11 -0400

    Update grades at time: 11-09-2020-13-05-56

---
+++ grade.js
@@ -0,0 +1,219 @@
+grade = 
+{
+  "yuzeren": {
+    "name": "Yu, Zeren", 
+    "unique_name": "yuzeren", 
+    "email": "yuzeren@umich.edu", 
+    "course": "EECS 367 F20", 
+    "repository": "git@github.com:frederic524288/Zstandard.git", 
+    "assignments": {
+      "Honor": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "PathPlan_Heap": {
+        "status": "PENDING", 
+        "test results": {
+          "Test 8": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR] wrong element when insert\n\nError when processing: 7766 4672 2506 8903 2816 6410 5271 1745 8406 3031 8421 756 2879 805 5355 8885 7006 9891 6124 2247 \n"
+          }, 
+          "Test 9": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR] wrong element when insert\n\nError when processing: 5457 2742 6577 9137 6629 2429 5942 1517 7359 3420 6966 6271 6443 6369 6443 7557 9998 793 8097 5115 \n"
+          }, 
+          "Test 2": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR] wrong element when insert\n\nError when processing: 6403 4272 5028 4142 2026 2718 9819 9580 877 1737 3782 6018 7425 9152 6354 5458 1562 2385 2504 9022 \n"
+          }, 
+          "Test 3": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR] wrong element when insert\n\nError when processing: 7611 7035 7774 4004 1634 9676 8575 9584 741 4195 7333 1874 7375 468 2991 9538 5978 2820 5980 3187 \n"
+          }, 
+          "Test 0": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR] wrong element when insert\n\nError when processing: 3394 8234 3770 3876 8321 8498 4731 8451 6616 516 9725 5056 5751 4352 2718 5536 1035 5653 2484 6601 \n"
+          }, 
+          "Test 1": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR] wrong element when insert\n\nError when processing: 416 7364 7578 7994 2143 9948 9668 9167 9245 1767 4798 8502 4088 8061 8262 5750 4886 5844 8039 7165 \n"
+          }, 
+          "Test 6": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR] wrong element when insert\n\nError when processing: 9346 9648 3032 8299 4991 9475 8955 5849 4549 7639 9815 7310 7435 1847 9471 8554 4346 8290 2523 9136 \n"
+          }, 
+          "Test 7": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR] wrong element when insert\n\nError when processing: 4338 1424 7233 2332 1834 7560 8404 3533 8988 8990 3317 4139 6045 240 8493 9475 7023 5218 7274 7660 \n"
+          }, 
+          "Test 4": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR] wrong element when insert\n\nError when processing: 5015 3741 4315 9964 5365 383 6012 522 3908 7931 5927 5119 8396 9111 6799 8090 3476 1240 7600 7440 \n"
+          }, 
+          "Test 5": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR] wrong element when insert\n\nError when processing: 1015 223 4255 8531 9379 1081 4372 8562 4071 4392 6767 4965 7195 5028 9395 3830 8748 1510 3348 8412 \n"
+          }
+        }, 
+        "comments": [
+          "0 out of 10 test cases passed"
+        ]
+      }, 
+      "PathPlan_AStar": {
+        "status": "PENDING", 
+        "test results": {
+          "Test 2": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }, 
+          "Test 3": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }, 
+          "Test 0": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }, 
+          "Test 1": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }, 
+          "Test 4": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }
+        }, 
+        "comments": [
+          "0 out of 5 test cases passed"
+        ]
+      }, 
+      "Pendularm_Euler": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "Pendularm_VelocityVerlet": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "Pendularm_PID": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FK_MatrixRoutines": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FK_Transforms": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FK_JointRendering": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FK_BaseOffset": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FSMDance_Quaternion": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FSMDance_RexArm": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FSMDance_SetpointControl": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FSMDance_FSM": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "IK_Jacobian": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "IK_JTranspose": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "IK_JPseudoInverse": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "MotionPlan_Collision": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "MotionPlan_2DRRTConnect": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "MotionPlan_CSpaceRRTConnect": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "Pitch": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }
+    }, 
+    "quizzes": [
+      {
+        "status": "PENDING"
+      }, 
+      {
+        "status": "PENDING"
+      }, 
+      {
+        "status": "PENDING"
+      }, 
+      {
+        "status": "PENDING"
+      }, 
+      {
+        "status": "PENDING"
+      }
+    ]
+  }
+}
