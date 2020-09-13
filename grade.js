8f47e5f8e2a91ceb4071e4694a93e1125e5523db
CI Grader <zhezhou@umich.edu>
2020-09-13 01:02:09 -0400

    Update grades at time: 13-09-2020-01-01-11

--- grade.js
+++ grade.js
@@ -14,51 +14,51 @@ grade =
         ]
       }, 
       "PathPlan_Heap": {
-        "status": "PENDING", 
+        "status": "PASS", 
         "test results": {
           "Test 8": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR] wrong element when insert\n\nError when processing: 2730 256 4636 9188 4975 5582 2012 963 4072 2260 5024 2027 4652 8986 7454 2605 8555 8403 3361 7193 \n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 9": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR] wrong element when insert\n\nError when processing: 7166 9155 7462 4402 3257 5860 5855 4595 7363 6559 3526 7864 4702 8568 8826 1796 1012 6956 8791 4404 \n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 2": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR] wrong element when insert\n\nError when processing: 6532 6670 6644 7847 8630 7268 8892 3125 8356 4694 828 1698 7084 8417 2233 6744 7297 4853 5251 5698 \n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 3": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR] wrong element when insert\n\nError when processing: 6654 1662 1487 9098 5248 690 7 3229 2613 7744 306 3605 5186 8605 4301 4800 9603 6636 3809 8289 \n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 0": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR] wrong element when insert\n\nError when processing: 3576 2241 487 4783 6556 5931 6984 575 5848 2283 4232 217 1915 200 8647 1689 8781 8036 5584 6064 \n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 1": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR] wrong element when insert\n\nError when processing: 508 5064 674 1047 7949 9434 2683 4071 7693 5837 8032 9553 1814 3067 8835 8214 8256 6161 1074 1188 \n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 6": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR] wrong element when insert\n\nError when processing: 3082 8104 7969 8985 4375 617 4186 8591 3018 323 2347 6587 3461 1693 8115 4093 9577 8257 729 9678 \n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 7": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR] wrong element when insert\n\nError when processing: 2224 2522 9885 4385 1060 171 3420 7009 3443 4599 4720 9332 8352 3609 4189 5223 9534 5748 5474 5759 \n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 4": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR] wrong element when insert\n\nError when processing: 1730 4668 622 4937 624 4801 8151 7920 6253 4271 8697 1767 6731 9384 7218 3001 4642 1650 9958 9659 \n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 5": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR] wrong element when insert\n\nError when processing: 3893 8999 2595 3479 9349 8044 5382 4417 218 9205 2829 998 3179 799 7087 7474 6394 5330 5716 6195 \n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }
         }, 
         "comments": [
-          "0 out of 10 test cases passed"
+          "10 out of 10 test cases passed"
         ]
       }, 
       "PathPlan_AStar": {
