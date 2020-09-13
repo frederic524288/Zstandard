ad0549d8b015cb4423049dd3ebfdea0449d3b304
yuzeren <44623508+frederic524288@users.noreply.github.com>
2020-09-12 22:04:51 -0400

    Heap is done

--- tutorial_heapsort/heap.js
+++ tutorial_heapsort/heap.js
@@ -27,7 +27,20 @@ minheaper = {};
 
 // define insert function for min binary heap
 function minheap_insert(heap, new_element) {
+    var eIntIdx = heap.length;
+    var prntIdx = Math.floor((eIntIdx - 1 ) / 2);
+    heap.push(new_element);
+    var heaped = (eIntIdx <= 0) || (heap[prntIdx] <= heap[eIntIdx]);
 
+    while(!heaped){
+        var tmp = heap[prntIdx];
+        heap[prntIdx] = heap[eIntIdx];
+        heap[eIntIdx] = tmp;
+
+        eIntIdx = prntIdx;
+        prntIdx = Math.floor((eIntIdx - 1)/2);
+        heaped = (eIntIdx <= 0) ||(heap[prntIdx] <= heap[eIntIdx]);
+    }
     // STENCIL: implement your min binary heap insert operation
 }
 
@@ -40,10 +53,63 @@ minheaper.insert = minheap_insert;
 
 // define extract function for min binary heap
 function minheap_extract(heap) {
+    var temp;
+    temp = heap[0];
+    heap[0] = heap[heap.length - 1];
+    heap[heap.length - 1] = temp;
+    heap.pop();
+    var inde = 0;
+    
+    while(2 * inde + 1< heap.length){
+        var j = 2 * inde + 1;
+        if(j + 1 >= heap.length){
+            if(heap[j] < heap[inde]){
+                var temp1 = heap[inde];
+                heap[inde] = heap[j];
+                heap[j] = temp1;
+            }
+            break;
+        }
+        else{
+            if((heap[j] > heap[j + 1]) && (heap[j] < heap[inde])){
+                j++
+                var temp1 = heap[inde];
+                heap[inde] = heap[j];
+                heap[j] = temp1;
+                inde = j;
+            }
+            else if((heap[j] > heap[j + 1]) && (heap[j + 1] < heap[inde])){
+                j++;
+                var temp1 = heap[inde];
+                heap[inde] = heap[j];
+                heap[j] = temp1;
+                inde = j;
+            }
+            else if((heap[j + 1] < heap[inde])){
+                var temp1 = heap[inde];
+                heap[inde] = heap[j];
+                heap[j] = temp1;
+                inde = j;
+            }
+            else if(heap[j] < heap[inde]){
+                var temp1 = heap[inde];
+                heap[inde] = heap[j];
+                heap[j] = temp1;
+                inde = j;
+            }
+            else{
+                break;
+            }  
+        }
+    }
+    
+    return temp;
+    
+
 
     // STENCIL: implement your min binary heap extract operation
 }
-
+minheaper.extract = minheap_extract;
 // assign extract function within minheaper object
 
     // STENCIL: ensure extract method is within minheaper object
