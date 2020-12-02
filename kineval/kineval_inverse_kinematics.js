627b15d5da635158c0b2a0a0c3178d6a148d6d2d
yuzeren <44623508+frederic524288@users.noreply.github.com>
2020-12-02 00:01:45 -0500

    robot.dx

--- kineval/kineval_inverse_kinematics.js
+++ kineval/kineval_inverse_kinematics.js
@@ -38,7 +38,25 @@ kineval.randomizeIKtrial = function randomIKtrial () {
     kineval.params.trial_ik_random.time = cur_time.getTime()-kineval.params.trial_ik_random.start.getTime();
 
     // STENCIL: see instructor for random time trial code
+    endeffector_world = matrix_multiply(robot.joints[robot.endeffector.frame].xform,robot.endeffector.position);
+
+    // compute distance of endeffector to target
+    kineval.params.trial_ik_random.distance_current = Math.sqrt(
+            Math.pow(kineval.params.ik_target.position[0][0]-endeffector_world[0][0],2.0)
+            + Math.pow(kineval.params.ik_target.position[1][0]-endeffector_world[1][0],2.0)
+            + Math.pow(kineval.params.ik_target.position[2][0]-endeffector_world[2][0],2.0) );
+
+    // if target reached, increment scoring and generate new target location
+    // KE 2 : convert hardcoded constants into proper parameters
+    if (kineval.params.trial_ik_random.distance_current < 0.01) {
+        kineval.params.ik_target.position[0][0] = 1.2*(Math.random()-0.5);
+        kineval.params.ik_target.position[1][0] = 1.2*(Math.random()-0.5)+1.5;
+        kineval.params.ik_target.position[2][0] = 0.7*(Math.random()-0.5)+0.5;
+        kineval.params.trial_ik_random.targets += 1;
+        textbar.innerHTML = "IK trial Random: target " + kineval.params.trial_ik_random.targets + " reached at time " + kineval.params.trial_ik_random.time;
+    }
 }
+
 
 kineval.iterateIK = function iterate_inverse_kinematics(endeffector_target_world, endeffector_joint, endeffector_position_local) {
 
@@ -47,16 +65,19 @@ kineval.iterateIK = function iterate_inverse_kinematics(endeffector_target_world
     // [NOTICE]: Please assign the following 3 variables to test against CI grader
 
     // ---------------------------------------------------------------------------
-    // robot.dx = []              // Error term
-    // robot.jacobian = []        // Jacobian matrix of current IK iteration
+    // robot.dx = []              // Error term, matrix size: 6 x 1, e.g., [[1],[1],[1],[0],[0],[0]]
+    // robot.jacobian = []        // Jacobian matrix of current IK iteration matrix size: 6 x N
     // robot.dq = []              // Joint configuration change term (don't include step length)  
     // ---------------------------------------------------------------------------
 
     // Explanation of above 3 variables:
     // robot.dq = T(robot.jacobian) * robot.dx  // where T(robot.jacobian) means apply some transformations to the Jacobian matrix, it could be Transpose, PseudoInverse, etc.
     // dtheta = alpha * robot.dq   // alpha: step length
-
-
+    var endeff = matrix_multiply(robot.joints[endeffector_joint].xform, endeffector_position_local);
+    robot.dx = [[],[],[],[0],[0],[0]];
+    robot.dx[0][0] = endeffector_target_world.position[0][0] - endeff[0][0];
+    robot.dx[1][0] = endeffector_target_world.position[1][0] - endeff[1][0];
+    robot.dx[2][0] = endeffector_target_world.position[2][0] - endeff[2][0];
 
 }
 
